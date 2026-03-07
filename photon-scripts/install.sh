7f84c438540e8e696251492ed582183a9809303c
Alex Resnick <adr8292@gmail.com>
2026-03-06 16:23:49 -0800

    Add Camera Fix Scripts (#122)

---
+++ photon-scripts/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+nano /opt/photonvision/restart-photon.sh
+chmod +x /opt/photonvision/restart-photon.sh
+nano /etc/systemd/system/photon-camera-fix.service
+systemctl daemon-reload
+systemctl enable photon-camera-fix
+systemctl restart photon-camera-fix
+systemctl status photon-camera-fix
